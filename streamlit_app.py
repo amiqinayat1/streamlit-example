7090ef8140558513f1c0c940ff66d37de02a68e0
amiqinayat1 <158330387+amiqinayat1@users.noreply.github.com>
2024-01-31 23:33:17 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,9 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+
 """
-###Sharoon App
+##Sharoon App
 """
 num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
 num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
